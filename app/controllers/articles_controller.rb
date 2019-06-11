c17e314c8baede7546dfc3b4980fcbd5f54764b5
Raffi Kudlac <raffi.kudlac@gmail.com>
2019-06-10 21:18:53 -0700

    Creating CRUD functionalities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, {only: [:update, :destroy, :show]}
+  before_action :set_article, {only: [:edit, :update, :destroy, :show]}
 
   def index
     @articles = Article.all
@@ -21,26 +21,31 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if (@article.update(article_params))
+      flash[:success] = "The article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
   def destroy
-    if (@article)
-      @article.destroy
-    end
+    @article.destroy
   end
 
   def show
-    if (!@article)
-      redirect_to 'index'
-    end
   end
 
   private
     def set_article
-      @article = Article.find(params[:id])
+      @article = Article.find_by_id(params[:id])
+      if (!@article)
+        flash[:danger] = "The requested article could not be found"
+        redirect_to articles_path
+      end
     end
 
     def article_params
